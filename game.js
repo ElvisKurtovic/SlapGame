1c9618a8ec72606919426c83b16de74517f9f75f
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-02-11 13:43:06 -0700

    Outline

---
+++ game.js
@@ -0,0 +1,20 @@
+/** NOTE
+ * Attack function reduces health by argument amount
+ * Health is dynamically displayed on page to reflect amount
+ * 
+ * Buttons onClick="attack( # )"
+ * 
+ * attack(damage)
+ * health reduced by amount
+ * get health element and update it's value
+ */
+
+
+
+
+let health = 100;
+
+function slap() {
+   health--
+   alert("Health: " + health)
+}
